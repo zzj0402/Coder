aa880aeca4a9da087619d34681e77bed86390a52
Zing <zzj0402@gmail.com>
2020-07-08 17:45:09 +1200

    Update test_median_of_two_sorted_arrays.py

--- ao/test_median_of_two_sorted_arrays.py
+++ ao/test_median_of_two_sorted_arrays.py
@@ -5,11 +5,12 @@ from to.median_of_two_sorted_arrays import Solution
 class TestMedianOfTwoSortedArrays(unittest.TestCase):
 
     def test_example_1(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual(Solution([1, 3], [2]), 2.0,
+                         "The median should be 2.0")
 
     def test_example_2(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        self.assertEqual(Solution([1, 2], [3, 4]), 2.5,
+                         "The median should be 2.5")
 
 
 if __name__ == '__main__':
