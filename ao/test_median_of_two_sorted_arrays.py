d4c1d112d7f6fd75b5f5a360f2a055bff0081fa2
Zing <zzj0402@gmail.com>
2020-07-09 20:23:02 +1200

    0 left or right

--- ao/test_median_of_two_sorted_arrays.py
+++ ao/test_median_of_two_sorted_arrays.py
@@ -14,6 +14,14 @@ class TestMedianOfTwoSortedArrays(unittest.TestCase):
         self.assertEqual(SOLUTION.findMedianSortedArrays([1, 2], [3, 4]), 2.5,
                          "The median should be 2.5")
 
+    def test_example_long(self):
+        self.assertEqual(SOLUTION.findMedianSortedArrays([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 6, 7, 8, 9]), 5.0,
+                         "The median should be 5.0")
+        self.assertEqual(SOLUTION.findMedianSortedArrays([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], [0]), 10.0,
+                         "The median should be 11.0")
+        self.assertEqual(SOLUTION.findMedianSortedArrays([0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]), 10.0,
+                         "The median should be 5.0")
+
 
 if __name__ == '__main__':
     unittest.main()
--- to/median_of_two_sorted_arrays.py
+++ to/median_of_two_sorted_arrays.py
@@ -8,20 +8,26 @@ class Solution:
         arranged_nums = []
         index_left = 0
         index_right = 0
-        while index_left < len(nums1) and index_right < len(nums2):
-            left = nums1[index_left]
-            right = nums2[index_right]
+        while index_left < len(nums1) or index_right < len(nums2):
+            if index_left < len(nums1):
+                left = nums1[index_left]
+            if index_right < len(nums2):
+                right = nums2[index_right]
             if left < right:
-                arranged_nums.append(left)
-                index_left += 1
-                if index_left == len(nums1):
+                if index_left >= len(nums1):
                     arranged_nums.append(right)
+                    index_right += 1
+                else:
+                    arranged_nums.append(left)
+                index_left += 1
             else:
-                arranged_nums.append(right)
+                if index_right >= len(nums2):
+                    arranged_nums.append(left)
+                    index_left += 1
+                else:
+                    arranged_nums.append(right)
                 index_right += 1
             if mod == 0:
-                print(arranged_nums)
-
                 if len(arranged_nums) > total_length/2:
                     return arranged_nums[-1]/2+arranged_nums[-2]/2
             else:
