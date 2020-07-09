038f43cbeaaa660e28460a2a61c5636e7e80e7a4
Zing <zzj0402@gmail.com>
2020-07-09 16:56:37 +1200

    Update median_of_two_sorted_arrays.py

--- to/median_of_two_sorted_arrays.py
+++ to/median_of_two_sorted_arrays.py
@@ -1,41 +1,22 @@
+from math import floor, ceil
+
+
 class Solution:
+    def getMean(self, nums):
+        if len(nums) % 2 == 0:
+            print("Even")
+            print(len(nums))
+            index_1 = floor(len(nums)/2)-1
+            print("index_1")
+            print(index_1)
+
+            index_2 = ceil(len(nums)/2)-1
+            mean = nums[index_1]/2+nums[index_2]/2
+            print(mean)
+        else:
+            mean = nums[int(len(nums)/2)]
+        return mean
+
     def findMedianSortedArrays(self, nums1, nums2) -> float:
-        A = nums1
-        B = nums2
-        m, n = len(A), len(B)
-        if m > n:
-            A, B, m, n = B, A, n, m
-        if n == 0:
-            raise ValueError
-
-        imin, imax, half_len = 0, m, (m + n + 1) / 2
-        while imin <= imax:
-            i = int((imin + imax) / 2)
-            j = int(half_len - i)
-            if i < m and B[j-1] > A[i]:
-                # i is too small, must increase it
-                imin = i + 1
-            elif i > 0 and A[i-1] > B[j]:
-                # i is too big, must decrease it
-                imax = i - 1
-            else:
-                # i is perfect
-
-                if i == 0:
-                    max_of_left = B[j-1]
-                elif j == 0:
-                    max_of_left = A[i-1]
-                else:
-                    max_of_left = max(A[i-1], B[j-1])
-
-                if (m + n) % 2 == 1:
-                    return max_of_left
-
-                if i == m:
-                    min_of_right = B[j]
-                elif j == n:
-                    min_of_right = A[i]
-                else:
-                    min_of_right = min(A[i], B[j])
-
-        return (max_of_left + min_of_right) / 2.0
+        print(self.getMean(nums1))
+        return self.getMean(nums1)/2 + self.getMean(nums2)/2
