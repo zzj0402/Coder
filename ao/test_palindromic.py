e66611c541f871198f0c6ce987d683e6fae6072d
Zing <zzj0402@gmail.com>
2020-07-11 17:14:59 +1200

    palindromic draft1

--- ao/test_palindromic.py
+++ ao/test_palindromic.py
@@ -5,8 +5,8 @@ from to.palindromic import Solution
 class TestPalindromic(unittest.TestCase):
 
     def test_example_1(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
+        S=Solution()
+        self.assertEqual(S.longestPalindrome("babad"),"bab","test case 1 should pass")
 
 if __name__ == '__main__':
     unittest.main()
--- to/palindromic.py
+++ to/palindromic.py
@@ -1,3 +1,21 @@
 class Solution:
+    def reverse_check(self,s):
+        index=len(s)-1
+        temp=''
+        while index>=0:
+            temp+=s[index]
+            index-=1
+        return True if temp==s else False
+
     def longestPalindrome(self, s: str) -> str:
-        return ""
+        result=''
+        start=0
+        length=1
+        end=start+length+1
+        while(start<len(s)):
+            while (end<=len(s)):
+                window=s[start:end]
+                result=window if len(window)>len(result) and self.reverse_check(window) else result
+                length+=1
+            start+=1
+        return result
