04af614fa3faf5442ef858ba3e724d1594b06f94
Zing <zzj0402@gmail.com>
2020-07-20 11:19:24 +1200

    negative

--- ao/test_reverse_integer.py
+++ ao/test_reverse_integer.py
@@ -6,8 +6,14 @@ SOLUTION = Solution()
 
 class TestReverseInteger(unittest.TestCase):
 
-    def test_(self):
+    def test_1(self):
         self.assertEqual(SOLUTION.reverse(123), 321)
+
+    def test_2(self):
+        self.assertEqual(SOLUTION.reverse(-123), -321)
+
+    def test_3(self):
+        self.assertEqual(SOLUTION.reverse(120), 21)
 
 
 if __name__ == '__main__':
--- to/reverse_integer.py
+++ to/reverse_integer.py
@@ -1,5 +1,13 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        if x < 0:
+            negative = True
+            x = abs(x)
+        else:
+            negative = False
         string = str(x)
         reverse = string[::-1]
-        return int(reverse)
+        if negative:
+            return -int(reverse)
+        else:
+            return int(reverse)
