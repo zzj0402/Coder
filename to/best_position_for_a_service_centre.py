ad96436a8e6b73986081d1e5d05e7e78e7dab7c0
William Huang <yilin.huang@mail.hypers.com>
2020-07-23 09:09:45 +0800

    feat(Best Position for a Service Centre): add main strategy

--- to/best_position_for_a_service_centre.py
+++ to/best_position_for_a_service_centre.py
@@ -1,13 +1,39 @@
 import math
 
+min_delta = 10 ** -5
+
 
 class Solution:
     def getMinDistSum(self, positions) -> float:
-        result = 0
-        x = 0
-        y = 0
-        # list formula then calc the min val of it
-        for i in range(len(positions)):
-            result += (positions[i][0] - x) ** 2 + (positions[i][1] - y) ** 2
+        def get_dist(x, y):
+            dist_result = 0
+            for i, j in positions:
+                dist_result += (x - i) ** 2 + (y - j) ** 2
+            return math.sqrt(dist_result)
 
-        return math.sqrt(result)
+        def get_initial_x_y():
+            y_init = x_init = 0
+            for i, j in positions:
+                x_init += i
+                y_init += j
+            return x_init / len(positions), y_init / len(positions)
+
+        x, y = get_initial_x_y()
+        step = 1
+        curr_dist = get_dist(x, y)
+        while step >= min_delta:
+            flag_to_division = True
+            for delta_x, delta_y in [(0, step), (0, -step), (step, 0), (-step, 0)]:
+                tmp_x = x + delta_x
+                tmp_y = y + delta_y
+                tmp_dist = get_dist(tmp_x, tmp_y)
+                if tmp_dist < curr_dist:
+                    # if the result is getting closer to the answer, not necessary to shorten the step
+                    flag_to_division = False
+                    curr_dist = tmp_dist
+                    x = tmp_x
+                    y = tmp_y
+
+            if flag_to_division:
+                step /= 10  # division by 10
+        return curr_dist
