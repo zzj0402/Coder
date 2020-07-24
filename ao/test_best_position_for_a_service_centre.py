54b83a17e09d509e0769d2a7bcd7b7cc1a7415ed
William Huang <yilin.huang@mail.hypers.com>
2020-07-24 08:56:40 +0800

    test(Best Position for a Service Centre): add testcase

--- ao/test_best_position_for_a_service_centre.py
+++ ao/test_best_position_for_a_service_centre.py
@@ -27,6 +27,9 @@ class TestServiceCenter(unittest.TestCase):
     def test_eg5(self):
         self.assert_equal_with_precision(SOLUTION.getMinDistSum([[0,1],[3,2],[4,5],[7,6],[8,9],[11,1],[2,12]]), 32.94036)
 
+    def test_complement1(self):
+        self.assert_equal_with_precision(SOLUTION.getMinDistSum([[4,4],[52,89],[76,60],[4,4],[4,4],[93,59],[50,92],[4,4],[76,14],[4,4],[46,41],[4,4],[4,4],[4,4],[4,4],[67,14],[73,71],[83,44],[4,4],[4,4],[4,4],[4,4],[30,29],[74,77],[4,4],[4,4],[4,4],[26,62],[4,4],[4,4],[50,30],[44,93]]), 1119.58582)
+
 
 if __name__ == '__main__':
     unittest.main()
