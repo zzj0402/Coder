4b22ca0b6039df90795cda862bba70a7df3f87e9
Zing <zzj0402@gmail.com>
2020-08-04 23:28:17 +1200

    Update longest_common_prefix.py

--- to/longest_common_prefix.py
+++ to/longest_common_prefix.py
@@ -1,3 +1,11 @@
 class Solution:
     def longestCommonPrefix(self, array_of_strings) -> str:
-        return''
+        output = ''
+        index = 0
+        while index < len(array_of_strings[0]):
+            last_string_character = array_of_strings[0][index]
+            for each_string in array_of_strings:
+                if last_string_character != each_string[index]:
+                    return output
+            output += last_string_character
+            index += 1
